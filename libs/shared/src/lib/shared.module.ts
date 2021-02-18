20a08355fc715b07635307ae3c332691b60af4c1
linbudu599 <linbudu599@gmail.com>
2021-02-18 21:58:40 +0800

    :sparkles(enhanced-todo): feat: title highlight directive

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CapitalizePipe } from './pipes.pipe';
+import { TitleHighlightDirective } from './directives.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CapitalizePipe],
-  exports: [CapitalizePipe],
+  declarations: [CapitalizePipe, TitleHighlightDirective],
+  exports: [CapitalizePipe, TitleHighlightDirective],
 })
 export class SharedModule {}
