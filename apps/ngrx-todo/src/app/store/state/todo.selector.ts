71e448492c0263fa4bf495973d6de26f5eed1de7
linbudu599 <linbudu599@gmail.com>
2021-03-12 11:10:05 +0800

    :zap: feat(ngrx-todo): update selectors

--- apps/ngrx-todo/src/app/store/state/todo.selector.ts
+++ apps/ngrx-todo/src/app/store/state/todo.selector.ts
@@ -1,26 +1,30 @@
-import {
-  createSelector,
-  createFeatureSelector,
-  props,
-  select,
-} from '@ngrx/store';
+import { createSelector, createFeatureSelector, select } from '@ngrx/store';
 
 import { from, pipe } from 'rxjs';
-import { every, filter, map } from 'rxjs/operators';
-
+import { filter, map } from 'rxjs/operators';
+import { TODO_FEATURE_KEY } from './todo.reducer';
 import { TodoModel } from './todo.model';
-import { TodoItemBase } from '@todoapp/dto';
 
-import * as todoReducer from './todo.reducer';
+// GlobalState >>> TodoModel
+export const selectTodoModel = createFeatureSelector<TodoModel>(
+  TODO_FEATURE_KEY
+);
 
-export const selectTodoModel = createFeatureSelector<TodoModel>('todo');
-
+// TodoModel >>> todos
 export const selectTodos = createSelector(
   selectTodoModel,
   (state: TodoModel) => state.todos
+);
+
+// with props
+export const selectTodoById = createSelector(
+  selectTodoModel,
+  (state: TodoModel, id: number) => state.todos.find((todo) => todo.id === id)
 );
 
 export const selectValidTodoOnly = pipe(
   select(selectTodos),
   map((todos) => from(todos).pipe(filter((todo) => !!todo.id)))
 );
+
+// selectTodoById.release()
